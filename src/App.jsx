cea25355107a5dae83a48e5735de6a1c988243ca
nichtwer <nichtwer@gmail.com>
2026-06-25 08:10:33 +0100

    Fix SVG namespace declaration in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ function App() {
               width="24"
               height="24"
               viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg
+              xmlns="http://www.w3.org/2000/svg"
               aria-label="How to play"
             >
               <path
