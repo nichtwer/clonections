9ff7598c76d7e67c135a5468d4c973177bc56e52
nichtwer <nichtwer@gmail.com>
2026-06-26 09:46:51 +0100

    Register service worker in index.jsx
    Added service worker registration to enhance app performance.

--- src/index.jsx
+++ src/index.jsx
@@ -12,7 +12,13 @@ root.render(
   </React.StrictMode>,
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// Register the custom Service Worker
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js').catch((error) => {
+      console.error('Service Worker registration failed:', error);
+    });
+  });
+}
